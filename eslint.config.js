0f85fc0e7a50259b4c728d3f16772447f3bd4622
Shay Cojocaru <shayc@outlook.com>
2025-09-10 22:08:11 +0300

    chore: update ESLint configuration and add eslint-config-prettier dependency

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import js from "@eslint/js";
-import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
@@ -20,4 +21,5 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  eslintConfigPrettier,
 ]);
