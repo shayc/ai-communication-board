a65943ac78503be8e3b150d2b716bed1ed9fc8cb
Shay Cojocaru <shayc@outlook.com>
2025-09-29 00:32:42 +0300

    feat: update SuggestionBar component to display food suggestions using Chips

--- src/components/CommunicationBoard/SuggestionBar/SuggestionBar.tsx
+++ src/components/CommunicationBoard/SuggestionBar/SuggestionBar.tsx
@@ -1,5 +1,17 @@
+import Chip from "@mui/material/Chip";
 import classes from "./SuggestionBar.module.css";
 
 export function SuggestionBar() {
-  return <div className={classes.root}>Suggestion bar</div>;
+  return (
+    <div className={classes.root}>
+      <Chip
+        label="I'm hungry, I'd like a sandwich"
+        onClick={() => alert("I'm hungry, I'd like a sandwich")}
+      />
+      <Chip
+        label="I'm hungry, I'd like a salad"
+        onClick={() => alert("I'm hungry, I'd like a salad")}
+      />
+    </div>
+  );
 }
