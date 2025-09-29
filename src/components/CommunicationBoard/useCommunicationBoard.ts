5a02e0b593e62dbd5d8ed65d7f699bb16ea7c73f
Shay Cojocaru <shayc@outlook.com>
2025-09-29 04:36:46 +0300

    Implement code changes to enhance functionality and improve performance

--- src/components/CommunicationBoard/useCommunicationBoard.ts
+++ src/components/CommunicationBoard/useCommunicationBoard.ts
@@ -1,7 +1,8 @@
-import type { Board } from "../../components/CommunicationBoard/types";
-import lotsOfStuff from "../../open-board-format/examples/lots_of_stuff.json";
+// import lotsOfStuff from "../../open-board-format/examples/lots_of_stuff.json";
+import projectCore from "../../open-board-format/examples/project-core.json";
+import type { Board } from "./types";
 
 export function useCommunicationBoard() {
-  const board = lotsOfStuff as Board;
+  const board = projectCore as unknown as Board;
   return board;
 }
