6ec596dae087c243452aa194f96847b6db8362b4
Shay Cojocaru <shayc@outlook.com>
2025-09-30 04:10:53 +0300

    feat: add sentiment icons and layout to SuggestionBar component

--- src/components/CommunicationBoard/SuggestionBar/SuggestionBar.tsx
+++ src/components/CommunicationBoard/SuggestionBar/SuggestionBar.tsx
@@ -1,5 +1,9 @@
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 import Chip from "@mui/material/Chip";
+import IconButton from "@mui/material/IconButton";
 import classes from "./SuggestionBar.module.css";
+import Box from "@mui/material/Box";
 
 export function SuggestionBar() {
   return (
@@ -12,6 +16,16 @@ export function SuggestionBar() {
         label="I'm hungry, I'd like a salad"
         onClick={() => alert("I'm hungry, I'd like a salad")}
       />
+
+      <Box sx={{ ml: "auto" }}>
+        <IconButton color="primary" aria-label="Satisfied tone">
+          <SentimentSatisfiedAltIcon />
+        </IconButton>
+
+        <IconButton color="primary" aria-label="Dissatisfied tone">
+          <SentimentVeryDissatisfiedIcon />
+        </IconButton>
+      </Box>
     </div>
   );
 }
