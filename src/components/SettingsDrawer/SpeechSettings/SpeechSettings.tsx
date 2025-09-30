e444925a879f477e2bd452b6de4aabd6963bdc25
Shay Cojocaru <shayc@outlook.com>
2025-09-30 03:42:11 +0300

    feat: enhance SpeechSettings component with improved volume slider layout and added value labels

--- src/components/SettingsDrawer/SpeechSettings/SpeechSettings.tsx
+++ src/components/SettingsDrawer/SpeechSettings/SpeechSettings.tsx
@@ -1,9 +1,12 @@
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import Button from "@mui/material/Button";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import Slider from "@mui/material/Slider";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import { useSpeech } from "../../../providers/SpeechProvider/SpeechProvider";
 
@@ -47,6 +50,7 @@ export function SpeechSettings() {
       <Slider
         disabled={!isSupported}
         aria-label="Pitch"
+        valueLabelDisplay="auto"
         value={pitch}
         min={0.1}
         max={2}
@@ -58,6 +62,7 @@ export function SpeechSettings() {
       <Slider
         disabled={!isSupported}
         aria-label="Rate"
+        valueLabelDisplay="auto"
         value={rate}
         min={0.1}
         max={2}
@@ -66,15 +71,20 @@ export function SpeechSettings() {
       />
 
       <Typography gutterBottom>Volume</Typography>
-      <Slider
-        disabled={!isSupported}
-        aria-label="Volume"
-        value={volume}
-        min={0}
-        max={1}
-        step={0.1}
-        onChange={(_event, value) => setVolume(value)}
-      />
+      <Stack spacing={2} direction="row" sx={{ alignItems: "center", mb: 1 }}>
+        <VolumeDownIcon />
+        <Slider
+          disabled={!isSupported}
+          aria-label="Volume"
+          valueLabelDisplay="auto"
+          value={volume}
+          min={0}
+          max={1}
+          step={0.1}
+          onChange={(_event, value) => setVolume(value)}
+        />
+        <VolumeUpIcon />
+      </Stack>
 
       <Button
         variant="contained"
