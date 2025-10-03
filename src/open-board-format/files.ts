c051f2be269e5cff5b5ce8e3ec04c5c49b457f42
Shay Cojocaru <shayc@outlook.com>
2025-10-03 04:12:14 +0300

    refactor: remove unused parameters from file import functions

--- src/open-board-format/files.ts
+++ src/open-board-format/files.ts
@@ -1,23 +1,23 @@
-import type { Unzipped } from "fflate";
-import { unzip } from "fflate";
-import mime from "mime/lite";
+// import type { Unzipped } from "fflate";
+// import { unzip } from "fflate";
+// import mime from "mime/lite";
 
 export function importBoardFile(file: File): Promise<void> {
   if (file.name.endsWith(".obz")) {
-    return importOBZFile(file);
+    return importOBZFile();
   }
 
   if (file.name.endsWith(".obf")) {
-    return importOBFFile(file);
+    return importOBFFile();
   }
 
   return Promise.reject(new Error("Unsupported file type"));
 }
 
-async function importOBZFile(file: File) {
-  
+async function importOBZFile() {
+  // TODO: Implement .obz file import
 }
 
-async function importOBFFile(file: File) {
-  // Implementation for importing .obf files
+async function importOBFFile() {
+  // TODO: Implement .obf file import
 }
